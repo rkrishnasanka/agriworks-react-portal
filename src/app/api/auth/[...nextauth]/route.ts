0ac493beaa39b552c77cef9665d4382e1ff560d6
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2024-07-15 17:25:30 +0530

    Updated all the code dependencies

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,7 @@
+// import NextAuth from "next-auth"
+
+// const handler = NextAuth({
+//     // Configure one or more authentication providers
+// })
+
+// export { handler as GET, handler as POST }
